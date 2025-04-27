3b786c69669078810471514971a0b9113e392f33
sultan <sultanhasanov987@gmail.com>
2025-04-28 02:42:08 +0300

    Добавил кнопку установить приложение5

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-
-const manifest = {
-  theme_color: "#8936FF",
-  background_color: "#2EC6FE",
-  icons: [
-    {
-      purpose: "maskable",
-      sizes: "512x512",
-      src: "/smart/icon512_maskable.png", // укажите путь с учетом подкаталога
-      type: "image/png",
-    },
-    {
-      purpose: "any",
-      sizes: "512x512",
-      src: "/smart/icon512_rounded.png", // укажите путь с учетом подкаталога
-      type: "image/png",
-    },
-  ],
-
-  orientation: "any",
-  display: "standalone",
-  lang: "ru-RU",
-  name: "Smart",
-  start_url: "/smart/", // Убедитесь, что путь правильный
-  screenshots: [
-    {
-      src: "/screenshot1.png",
-      sizes: "1280x800",
-      type: "image/png",
-      form_factor: "wide",
-    },
-    {
-      src: "/screenshot2.png",
-      sizes: "640x1136",
-      type: "image/png",
-    },
-  ],
-};
+import manifest from "./public/manifest.json"; // импортируем манифест
 
 export default defineConfig({
-  base: "/smart/", // 👈 обязательно укажи путь как у репозитория
+  base: "/smart/",
   plugins: [
     react(),
     VitePWA({
@@ -49,7 +12,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg,woff2,woff}"],
       },
-      manifest: manifest,
+      manifest: manifest, // используем манифест
     }),
   ],
 });
