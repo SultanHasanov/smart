0f99391f64f9a85dee4fb88377d2a5e5dc6e9690
sultan <sultanhasanov987@gmail.com>
2025-04-28 02:22:08 +0300

    Добавил кнопку установить приложение5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { Button } from "antd"; // Импортируем компонент Button из Ant Design
+import React, { useEffect } from "react";
 import Product from "./component/Product";
 import Header from "./component/Header";
 import OfflineDetector from "./component/OfflineDetector";
@@ -7,62 +6,27 @@ import DevTerminal from "./component/DevTerminal";
 import SiteBanner from "./component/SiteBanner";
 
 const App = () => {
-  const [deferredPrompt, setDeferredPrompt] = useState(null);
-  const [showInstallButton, setShowInstallButton] = useState(false);
 
-  // Логика для отображения кнопки установки
   useEffect(() => {
     const handleBeforeInstallPrompt = (e) => {
-      // Предотвращаем стандартное поведение
       e.preventDefault();
-      setDeferredPrompt(e); // Сохраняем событие
-      setShowInstallButton(true); // Показываем кнопку
+      setDeferredPrompt(e);
+      setShowInstallButton(true);
     };
-
-    // Проверяем, показывать ли кнопку только в первый раз
-    if (!localStorage.getItem('isAppInstalled')) {
-      window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
-    }
-
+  
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+  
     return () => {
       window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
     };
   }, []);
-
-  // Функция для обработки нажатия на кнопку установки
-  const handleInstall = () => {
-    if (deferredPrompt) {
-      deferredPrompt.prompt(); // Показываем диалог установки
-      deferredPrompt.userChoice
-        .then((choiceResult) => {
-          if (choiceResult.outcome === "accepted") {
-            localStorage.setItem('isAppInstalled', 'true'); // Запоминаем, что приложение установлено
-          } else {
-            // Можно добавить уведомление о том, что установка отклонена
-          }
-          setDeferredPrompt(null); // Очищаем событие
-          setShowInstallButton(false); // Скрываем кнопку
-        });
-    }
-  };
-
+  
   return (
     <div>
       <OfflineDetector />
       <SiteBanner />
       <Product />
-      <DevTerminal />
-
-      {showInstallButton && (
-        <Button
-          type="primary"
-          size="large"
-          onClick={handleInstall}
-          style={{ position: "fixed", bottom: "20px", right: "20px", zIndex: 1000 }}
-        >
-          Установить приложение
-        </Button>
-      )}
+      <DevTerminal/>
     </div>
   );
 };
