4e4a3058d62dc481e41622c14ab6a38f325c630a
sultan <sultanhasanov987@gmail.com>
2025-04-28 02:50:20 +0300

    Добавил кнопку

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Button } from "antd"; 
 import Product from "./component/Product";
 import Header from "./component/Header";
 import OfflineDetector from "./component/OfflineDetector";
@@ -6,6 +7,8 @@ import DevTerminal from "./component/DevTerminal";
 import SiteBanner from "./component/SiteBanner";
 
 const App = () => {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [showInstallButton, setShowInstallButton] = useState(false);
 
   useEffect(() => {
     const handleBeforeInstallPrompt = (e) => {
@@ -13,20 +16,51 @@ const App = () => {
       setDeferredPrompt(e);
       setShowInstallButton(true);
     };
-  
+
     window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
-  
+
     return () => {
       window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
     };
   }, []);
-  
+
+  const handleInstallClick = async () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      const { outcome } = await deferredPrompt.userChoice;
+      if (outcome === "accepted") {
+        console.log("Пользователь установил приложение");
+      } else {
+        console.log("Пользователь отказался от установки");
+      }
+      setDeferredPrompt(null);
+      setShowInstallButton(false);
+    }
+  };
+
   return (
     <div>
+      <Header />
       <OfflineDetector />
       <SiteBanner />
       <Product />
-      <DevTerminal/>
+      <DevTerminal />
+
+      {showInstallButton && (
+        <Button
+          type="primary"
+          size="large"
+          style={{
+            position: "fixed",
+            bottom: "20px",
+            right: "20px",
+            zIndex: 1000,
+          }}
+          onClick={handleInstallClick}
+        >
+          Установить приложение
+        </Button>
+      )}
     </div>
   );
 };
