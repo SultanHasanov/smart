7952dca3f47c18fd52351357f965b3f1248fe4db
sultan <sultanhasanov987@gmail.com>
2025-04-30 01:18:40 +0300

    dynamic base path for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import manifest from "./public/manifest.json"; // импортируем манифест
+import manifest from "./public/manifest.json";
+
+const branch = process.env.GITHUB_REF_NAME;
+const base = branch === "dev" ? "/smart/dev/" : "/smart/";
 
 export default defineConfig({
-  base: "/smart/",
+  base,
   plugins: [
     react(),
     VitePWA({
@@ -12,7 +15,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg,woff2,woff}"],
       },
-      manifest: manifest, // используем манифест
+      manifest,
     }),
   ],
 });
