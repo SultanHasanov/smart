3fad5646ac77c374e486ffa7d468ff04a4ce7dff
sultan <sultanhasanov987@gmail.com>
2025-05-21 03:52:39 +0300

    Сделал страницу 404

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,15 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import manifest from "./public/manifest.json";
 
-
-
 export default defineConfig({
-   base: '/',
+  base: "/",
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
       workbox: {
         globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg,woff2,woff}"],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
       manifest,
     }),
